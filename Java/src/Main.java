014d79d1805f5ff07aa33ad72f22bbe1b3031d10
yena <w2302@e-mirim.hs.kr>
2024-02-20 02:24:26 +0900

    함수

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,8 +1,19 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(add(a,b));
         }
-    
+        static int add (int x, int y){
+            int result = x+y;
+            return result;
+    }
+
 }
